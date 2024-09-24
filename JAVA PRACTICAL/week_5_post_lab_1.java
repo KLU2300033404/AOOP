124dc4623e6745480a31659512f178b3335a645e
KLU2300033404 <163638964+KLU2300033404@users.noreply.github.com>
2024-09-24 13:21:54 +0530

    Update week_5_post_lab_1.java

--- JAVA PRACTICAL/week_5_post_lab_1.java
+++ JAVA PRACTICAL/week_5_post_lab_1.java
@@ -1,3 +1,27 @@
-public class week_5_post_lab_1 {
+package com.FactoryPattern;
 
+public class PineapplePizza extends Pizza{
+	private String name = "Pineapple Pizza";
+    private String dough = "Soft and Strechy";
+    private String sauce = "Tomato Sauce";
+    
+    public PineapplePizza() {
+    	toppings.add("Grated Reggiano Cheese");
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public String getDough() {
+    	return dough;
+    }
+    
+    public String getSauce() {
+    	return sauce;
+    }
+    
+    public void cut() {
+    	System.out.println("Cut the pizza into equal pieces");
+    }
 }
