24e509e09c24dc5d278ded1afbc02b5cb77f2132
KLU2300033404 <163638964+KLU2300033404@users.noreply.github.com>
2024-09-24 13:19:29 +0530

    Update week_11_in_lab_2.java

--- JAVA PRACTICAL/week_11_in_lab_2.java
+++ JAVA PRACTICAL/week_11_in_lab_2.java
@@ -1,3 +1,29 @@
-public class week_11_in_lab_2 {
+package com.log;
 
+public class logger {
+	
+	private static logger loggerInstance;
+
+    private logger() {
+      
+        System.out.println("Logger Initialized.");
+    }
+
+    public static logger getInstance() {
+     
+        if (loggerInstance == null) {
+            loggerInstance = new logger();
+        }
+        return loggerInstance;
+    }
+
+  
+    public void log(String message) {
+       
+        System.out.println("Log Message: " + message);
+    }
 }
+
+
+
+	
